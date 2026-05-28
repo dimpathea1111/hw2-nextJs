e3eba5a1689a31ffe5a7374d6110c3a5955df8f5
pathea <pathea.dim@gmail.com>
2026-05-28 09:58:34 +0700

    add

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,24 +1,40 @@
-import { error } from "console";
+// import { error } from "console";
+// import { NextResponse } from "next/server";
+
+
+// export async function GET(){
+//     //logic of fetching data
+//     const response=await fetch(process.env.NEXT_PUBLIC_BASE_ISHOP_API_URL+'/product');
+
+//     const data=await response.json();
+//     return NextResponse.json(data);
+//     if(response?.ok){
+//         return NextResponse.json({
+//  Success:true,
+//         data
+//         });
+       
+//     }
+
+
+
+//     return  NextResponse.json({
+//         error:'sorry u cannot fetch product data'
+//     }, {status:500});
+// }
+
+
+
 import { NextResponse } from "next/server";
 
-
 export async function GET(){
-    //logic of fetching data
-    const response=await fetch(process.env.NEXT_PUBLIC_BASE_ISHOP_API_URL+'/product');
-
-    const data=await response.json();
-    return NextResponse.json(data);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_ISHOP_API_URL}/products?size=36`);
+    const data = await response.json();
     if(response?.ok){
         return NextResponse.json({
- Success:true,
-        data
-        });
-       
+            success:true,
+            data
+        })
     }
-
-
-
-    return  NextResponse.json({
-        error:'sorry u cannot fetch product data'
-    }, {status:500});
+    return NextResponse.json({error:"Failed to fetch product data."},{status:500})
 }
--- src/app/components/dashboard/AsideComponentDashboard.tsx
+++ src/app/components/dashboard/AsideComponentDashboard.tsx
@@ -1,41 +1,4 @@
 
-// "use client";
-
-// import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems } from "flowbite-react";
-// import { usePathname } from "next/navigation";
-// import {  HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
-
-// export function AsideComponentDashboard() {
-
-//     const pathName=usePathname();
-
-//   return (
-//     <Sidebar aria-label="Default sidebar example">
-//       <SidebarItems>
-//         <SidebarItemGroup>
-//           <SidebarItem href="#" icon={HiChartPie}>
-//             Dashboard
-//           </SidebarItem>
-      
-//           <SidebarItem href="/dashboard/blogs" icon={HiUser} className={pathName === "/dashboard/blogs" ? "bg-blue-500 rounded" : ""}>
-//             Blog
-//           </SidebarItem>
-//           <SidebarItem href="/dashboard/products" icon={HiShoppingBag}  className={pathName === "/dashboard/products" ? "bg-blue-500 rounded" : ""}>
-//             Products
-//           </SidebarItem>
-//             <SidebarItem href="/dashboard/setting" icon={HiShoppingBag}  className={pathName === "/dashboard/setting" ? "bg-blue-500 rounded" : ""}>
-//             Setting
-//           </SidebarItem>
-         
-//         </SidebarItemGroup>
-//       </SidebarItems>
-//     </Sidebar>
-//   );
-// }
-
-
-
-
 "use client";
 
 import {
